4885d0d902f5d6d4de8c55f89ddc4c264338c816
calopez <calopez@gmail.com>
2024-08-23 16:25:03 -0600

    Class 30 of day 3 completed successfully

---
+++ Udemy/Backend/Python/Day_03/04_string_characteristics.py
@@ -0,0 +1,19 @@
+name = "Carina"
+name = "Karina"
+
+print(name)
+
+n1 = "Kari"
+n2 = "na"
+
+print(n1 + n2)
+print(n1 * 10)
+
+
+poema = """Mil pequeños peces blancos
+como si hirviera
+el color del agua"""
+
+print("agua" not in poema)
+
+print(len(poema))
